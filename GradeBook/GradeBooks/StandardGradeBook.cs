708ffd9894d34d95da1f5c9aec89197bd47c0bff
Michael <michael.orange@moodinternational.com>
2018-03-12 10:31:51 +0000

    2.3-2.6

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Standard;
+        }
+
+    }
+}
